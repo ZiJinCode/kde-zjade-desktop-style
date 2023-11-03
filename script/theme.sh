39953becd449a61c9223d8b2b1fd20fd64a4f9fc
zjade <2416844857@qq.com>
2023-11-03 16:54:28 +0800

    1

--- script/theme.sh
+++ script/theme.sh
@@ -1,129 +1,63 @@
 #!/bin/sh
 
-# Commands
+# ========== Setting Variable ==========
+WINDOW_STYLE_NAME="RoundedSBE"
+# ======================================
+
+# --------------------------------------
+
+# ---------- Main Variable ----------
+
+MAIN_PATH="$(dirname $(cd $(dirname $0);pwd))"
+SCRIPT_PATH="$MAIN_PATH/script"
+SCRIPT_FILE_PATH="$SCRIPT_PATH/$(basename $0)"
+
+THEME_PATH="$MAIN_PATH/theme"
+
 declare -A properties
 commands=()
-commands[0]="--noinstall"
-commands[1]="--local"
+commands[0]='--local'
+commands[1]='--noinstall'
 
-# Setting ==========================================
-# window style name
-WINDOW_STYLE_NAME="RoundedSBE"
-# ==================================================
+# -----------------------------------
 
-# --------------------------------------------------
-this_script_path=$0
-script_path=$(cd $(dirname $0);pwd)
-main_path=$(dirname $script_path)
-# --------------------------------------------------
 
-printHelp() {
-
+# -------------- Base Function --------------
+printVariable() {
+    echo ">>> ==================================="
+    echo ">>> MAIN_PATH: $MAIN_PATH"
+    echo ">>> SCRIPT_PATH: $SCRIPT_PATH"
+    echo ">>> SCRIPT_FILE_PATH: $SCRIPT_FILE_PATH"
+    echo ">>> ==================================="
 }
 
-printBasePath() {
-    echo "============================="
-    echo "Base Path: "
-    echo "this_script_path: $this_script_path"
-    echo "script_path: $script_path"
-    echo "main_path: $main_path"
-    echo -e "=============================\n"
+printProperties() {
+    # Print Properties
+    echo ">>> ==================================="
+    for key in ${!properties[@]};do
+        echo "K: $key -> V: ${properties["$key"]}}"
+    done
+    echo ">>> ==================================="
 }
 
-installSoft() {
-    echo "depend pacman"
-    echo "Install >>>>>>>>>>>>>>>"
-    sudo pacman -Syy kvantum
-}
-
-check_params() {
+# param1: command
+check_command() {
+    local i_command=$1
     for command in ${commands[@]};do
-        if [[ $command == $1 && $command != '' ]];then
+        if [[ $command == $i_command ]];then
             return 0
         fi
     done
     return 1
 }
 
-# param home_apth
-localInstallTheme() {
-    local home_path=$1
-    local local_share_path=$home_path/.local/share
-    # Icon
-    # cp to /usr/share/icons/
-    find $main_path/theme/icons/* -mindepth 1 -type d ! -name './' -exec cp -Rf {} $local_share_path/icons/{}"_icon" \;
-
-    # Cursors
-    # cp to /usr/share/icons/*-cursors
-    find $main_path/theme/cursors/* -mindepth 1 -type d ! -name './' -exec cp -Rf {} $local_share_path/icons/{}"_cursors" \;
-
-    # plasma Theme
-    # cp to /usr/share/plasma/desktoptheme
-    cp $main_path/theme/plasma_theme/* $local_share_path/plasma/desktoptheme
+# param1: key
+# param2: value
+setProperty() {
+    properties["$1"]=$2
 }
 
-globalInstallTheme() {
-    # Icon
-    # cp to /usr/share/icons/
-    find $main_path/theme/icons/* -mindepth 1 -type d ! -name './' -exec cp -Rf {} /usr/share/icons/{}"_icon" \;
-
-    # Cursors
-    # cp to /usr/share/icons/*-cursors
-    find $main_path/theme/cursors/* -mindepth 1 -type d ! -name './' -exec cp -Rf {} /usr/share/icons/{}"_cursors" \;
-
-    # plasma Theme
-    # cp to /usr/share/plasma/desktoptheme
-    cp $main_path/theme/plasma_theme/* /usr/share/plasma/desktoptheme
-}
-
-shareInstallTheme() {
-    # gtk_theme
-    # cp to /usr/share/themes
-    cp $main_path/theme/gtk_theme/* /usr/share/themes
-
-    # sddm
-    # /usr/share/sddm/themes
-    cp $main_path/theme/sddm_theme/* /usr/share/sddm/themes
-
-    # grub
-    # /usr/share/grub/themes
-    cp $main_path/theme/grub_theme/* /usr/share/grub/themes
-
-    # Fonts
-    # /usr/share/fonts
-    cp $main_path/theme/fonts/* /usr/share/fonts
-    fc-cache -f -v
-}
-
-kvantumInstall() {
-    # Kvantum manager
-    isExistKey '--noinstall'
-    if [[ $? == '0' ]];then
-        installSoft
-    else 
-        echo "Jmp Install"
-    fi
-}
-
-windowStyleInstall() {
-    # window style
-    sudo rm -rf /opt/local/kdemodule/$WINDOW_STYLE_NAME
-    mkdir -p /opt/local/kdemodule
-    cp -Rf $main_path/theme/window_style/$WINDOW_STYLE_NAME /opt/local/kdemodule/
-    cd /opt/local/kdemodule/$WINDOW_STYLE_NAME
-    sudo ./install.sh
-}
-
-setKeyValue() {
-    local key=$1
-    local value=$2
-    if [ -z $value ];then
-        properties["$key"]='0'
-    else
-        properties["$key"]=$value
-    fi
-}
-
+# param1: key
 isExistKey() {
     local i_key=$1
     for key in ${!properties[@]};do
@@ -133,35 +67,140 @@ isExistKey() {
     done
     return 1
 }
-# ---------------------------------------------------------
-# Main
-if [ $1 == 'help' ];then
-    printHelp
-    exit 0
-fi
 
-## init properties
-while [[ $# -gt 0 ]];do
-    # Check Key
-    check_params $1
+# -------------------------------------------
+
+# param Home_Path
+localInstallTheme() {
+    local HOME_PATH=$1
+    local SHARE_PATH="$HOME_PATH/.local/share"
+
+    # Icon
+    # cp to /usr/share/icons/
+    for dir_path in "$THEME_PATH/icons"/*;do
+        new_dir_name="$(basename $dir_path)-icon"
+        cp -Rf $dir_path $SHARE_PATH/icons/$new_dir_name
+    done
+
+    # Cursors
+    # cp to /usr/share/icons/*-cursors
+    for dir_path in "$THEME_PATH/cursors"/*;do
+        new_dir_name="$(basename $dir_path)-cursors"
+        cp -Rf $dir_path $SHARE_PATH/icons/$new_dir_name
+    done
+
+    # plasma Theme
+    # cp to /usr/share/plasma/desktoptheme
+    for dir_path in "$THEME_PATH/plasma_theme"/*;do
+        cp -Rf $dir_path $SHARE_PATH/icons/
+    done
+}
+
+globalInstallTheme() {
+    # Icon
+    # cp to /usr/share/icons/
+    for dir_path in "$THEME_PATH/icons"/*;do
+        new_dir_name="$(basename $dir_path)-icon"
+        sudo cp -Rf $dir_path /usr/share/icons/$new_dir_name
+    done
+
+    # Cursors
+    # cp to /usr/share/icons/*-cursors
+    for dir_path in "$THEME_PATH/cursors"/*;do
+        new_dir_name="$(basename $dir_path)-cursors"
+        sudo cp -Rf $dir_path /usr/share/icons/$new_dir_name
+    done
+
+    # plasma Theme
+    # cp to /usr/share/plasma/desktoptheme
+    for dir_path in "$THEME_PATH/plasma_theme"/*;do
+        sudo cp -Rf $dir_path /usr/share/plasma/desktoptheme/
+    done
+}
+
+shareInstallTheme() {
+    # gtk_theme
+    # cp to /usr/share/themes
+    sudo cp -Rf $THEME_PATH/gtk_theme/* /usr/share/themes
+
+    # sddm
+    # /usr/share/sddm/themes
+    sudo cp -Rf $THEME_PATH/sddm_theme/* /usr/share/sddm/themes
+
+    # grub
+    # /usr/share/grub/themes
+    sudo cp -Rf $THEME_PATH/grub_theme/* /usr/share/grub/themes
+
+    # Fonts
+    # /usr/share/fonts
+    sudo cp -Rf $THEME_PATH/fonts/* /usr/share/fonts
+    sudo fc-cache -f -v
+}
+
+# param HomePath
+kvantumInstall() {
+    local HOME_PATH=$1
+    # Kvantum manager
+    isExistKey '--noinstall'
     if [[ $? == '0' ]];then
-        key=$param
-    else
-        echo "输入s错误"
+        echo "depend pacman"
+        echo "Install >>>>>>>>>>>>>>>>>>>>>>>>"
+        sudo pacman -Syy kvantum
+    else 
+        echo "Jmp Install"
+    fi
+
+    if [ ! -d "$HOME_PATH/.config/Kvantum" ];then
+        ehco "!!!ERROR: 不存在!!! $HOME_PATH/.config/Kvantum"
         exit 1
     fi
 
-    # Check Value
-    check_params $2
+    sudo cp -Rf $THEME_PATH/kvantum_theme/* $HOME_PATH/.config/Kvantum
+}
+
+windowStyleInstall() {
+    # window style
+    if [ ! -d "/opt/local/kdemodule" ];then
+        sudo mkdir -p /opt/local/kdemodule
+    fi
+
+    if [ -d "/opt/local/kdemodule/$WINDOW_STYLE_NAME" ];then
+        sudo rm -rf /opt/local/kdemodule/$WINDOW_STYLE_NAME
+    fi
+
+    sudo cp -Rf $THEME_PATH/window_style/$WINDOW_STYLE_NAME /opt/local/kdemodule/
+
+    cd /opt/local/kdemodule/$WINDOW_STYLE_NAME
+    sudo ./install.sh
+}
+
+# Main --------------------------------
+
+## Init Properties
+while [[ $# -gt 0 ]];do
+    # Key
+    check_command $1
+    if [[ $? == '0' ]];then
+        key=$1
+    else
+        echo "没有这个命令"
+        exit 1
+    fi
+
+    # Value
+    check_command $2
     if [[ $? == '0' ]];then
         value=''
     else
-        value=$2
+        if [[ -z $2 ]];then
+            value=''
+        else
+            value=$2
+        fi
     fi
-        
-    setKeyValue $key $value
-    if [[ -z $value ]];then; shift 1; else shift 2; fi
-    done
+
+    setProperty $key $value
+    if [[ -z $value ]];then shift 1; else shift 2; fi 
 done
 
 ## Main Flow
